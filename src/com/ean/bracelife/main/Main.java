ed8c81eb9ccdd69a07b0407596b520caecc021f8
Carlos Bernal <carlos.mario.bernal@accenture.com>
2017-06-11 09:40:34 -0500

    Se agrega la librería de logs

--- src/com/ean/bracelife/main/Main.java
+++ src/com/ean/bracelife/main/Main.java
@@ -1,12 +1,18 @@
 package com.ean.bracelife.main;
 
+import org.apache.log4j.Logger;
+
 import com.ean.bracelife.arduino.Conexion;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-			System.out.print("Te amo");
-			Conexion conexion = new Conexion();
+		Logger logger = Logger.getLogger(Main.class);
+		
+		logger.info("Te amo!");
+
+		Conexion conexion = new Conexion();
 			System.out.print("Te amo");
 			
 	}
