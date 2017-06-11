311e7f2c80bbb086b92b5c4791e189800c057746
Carlos Bernal <carlos.mario.bernal@accenture.com>
2017-06-11 09:28:08 -0500

    Modificación de las rutas

---
+++ src/com/ean/bracelife/main/Main.java
@@ -0,0 +1,14 @@
+package com.ean.bracelife.main;
+
+import com.ean.bracelife.arduino.Conexion;
+
+public class Main {
+
+	public static void main(String[] args) {
+			System.out.print("Te amo");
+			Conexion conexion = new Conexion();
+			System.out.print("Te amo");
+			
+	}
+
+}
